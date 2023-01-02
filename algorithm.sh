afa10fa954df7e89b3a4a21313b58e71aef87f3c
Bora Kuşdoğan <82846187+Snowianwolf@users.noreply.github.com>
2023-01-02 12:03:15 +0300

    Update algorithm.sh

--- algorithm.sh
+++ algorithm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "MODİFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION" | tee reporttxt
+echo "MODİFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION" | tee report.txt
 echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
